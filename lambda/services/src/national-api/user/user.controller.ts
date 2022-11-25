1da96901a6779cdebf4eeff882777eba971fb7b4
palindaa <palinda@xeptagon.com>
2022-11-25 14:34:24 +0530

    Fix http code text

--- lambda/services/src/national-api/user/user.controller.ts
+++ lambda/services/src/national-api/user/user.controller.ts
@@ -33,7 +33,7 @@ export class UserController {
     @Post('add')
     addUser(@Body()user: UserDto) {
       if (user.role == Role.Root) {
-        throw new HttpException("Unauthorized", HttpStatus.FORBIDDEN)
+        throw new HttpException("Forbidden", HttpStatus.FORBIDDEN)
       }
       return this.userService.create(user)
     }
