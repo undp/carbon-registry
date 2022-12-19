30c29e4e485e819c0325e1fbe1e161a8a70ec645
palindaa <palinda@xeptagon.com>
2022-12-19 17:13:32 +0530

    Company view table

---
+++ web/src/Pages/CompanyManagement/companyManagement.tsx
@@ -0,0 +1,271 @@
+import {
+  BankOutlined,
+  DeleteOutlined,
+  EditOutlined,
+  EllipsisOutlined,
+  ExperimentOutlined,
+  EyeOutlined,
+  PlusOutlined,
+  SafetyOutlined,
+  SearchOutlined,
+  StarOutlined,
+  ToolOutlined,
+} from '@ant-design/icons';
+import {
+  Button,
+  Col,
+  Empty,
+  List,
+  message,
+  PaginationProps,
+  Popconfirm,
+  Popover,
+  Row,
+  Space,
+  Table,
+  Typography,
+} from 'antd';
+import React, { useEffect, useState } from 'react';
+import { PencilSquare, Trash } from 'react-bootstrap-icons';
+import './companyManagement.scss';
+import '../Common/common.table.scss';
+import { useNavigate } from 'react-router-dom';
+import type { ColumnsType } from 'antd/es/table';
+import { TableDataType } from '../../Definitions/InterfacesAndType/userManagement.definitions';
+import { useConnection } from '../../Context/ConnectionContext/connectionContext';
+import RoleIcon from '../../Components/RoleIcon/role.icon';
+import {
+  AdminBGColor,
+  AdminColor,
+  CertBGColor,
+  CertColor,
+  DevBGColor,
+  DevColor,
+  GovBGColor,
+  GovColor,
+  ManagerBGColor,
+  ManagerColor,
+  RootBGColor,
+  RootColor,
+  ViewBGColor,
+  ViewColor,
+} from '../Common/role.color.constants';
+import ProfileIcon from '../../Components/ProfileIcon/profile.icon';
+
+const CompanyManagement = () => {
+  const navigate = useNavigate();
+  const { get, delete: del } = useConnection();
+  const [totalCompany, setTotalCompany] = useState<number>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [tableData, setTableData] = useState<TableDataType[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
+
+  const getCompanyBgColor = (item: string) => {
+    if (item === 'Government') {
+      return GovBGColor;
+    } else if (item === 'Certifier') {
+      return CertBGColor;
+    }
+    return DevBGColor;
+  };
+
+  const getRoleComponent = (item: string) => {
+    return (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        {item === 'Admin' ? (
+          <RoleIcon icon={<StarOutlined />} bg={AdminBGColor} color={AdminColor} />
+        ) : item === 'Root' ? (
+          <RoleIcon icon={<SearchOutlined />} bg={RootBGColor} color={RootColor} />
+        ) : item === 'Manager' ? (
+          <RoleIcon icon={<ToolOutlined />} bg={ManagerBGColor} color={ManagerColor} />
+        ) : (
+          <RoleIcon icon={<EyeOutlined />} bg={ViewBGColor} color={ViewColor} />
+        )}
+        <div>{item}</div>
+      </div>
+    );
+  };
+
+  const getCompanyRoleComponent = (item: string) => {
+    return (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        {item === 'Government' ? (
+          <RoleIcon icon={<BankOutlined />} bg={GovBGColor} color={GovColor} />
+        ) : item === 'Certifier' ? (
+          <RoleIcon icon={<SafetyOutlined />} bg={CertBGColor} color={CertColor} />
+        ) : (
+          <RoleIcon icon={<ExperimentOutlined />} bg={DevBGColor} color={DevColor} />
+        )}
+        {item === 'ProgrammeDeveloper' ? <div>{'Developer'}</div> : <div>{item}</div>}
+      </div>
+    );
+  };
+
+  const actionMenu = (record: TableDataType) => {
+    return (
+      <List
+        className="action-menu"
+        size="small"
+        dataSource={[
+          {
+            text: 'Edit',
+            icon: <EditOutlined />,
+            click: () => {
+              navigate('/companyManagement/updateCompany', { state: { record } });
+            },
+          },
+          { text: 'Delete', icon: <DeleteOutlined />, click: () => {} },
+        ]}
+        renderItem={(item) => (
+          <List.Item onClick={item.click}>
+            <Typography.Text className="action-icon">{item.icon}</Typography.Text>
+            <span>{item.text}</span>
+          </List.Item>
+        )}
+      />
+    );
+  };
+
+  const columns = [
+    {
+      title: 'COMPANY NAME',
+      dataIndex: 'name',
+      key: 'name',
+      align: 'left' as const,
+      render: (item: any, itemObj: any) => {
+        return (
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <ProfileIcon
+              icon={itemObj.logo}
+              bg={getCompanyBgColor(itemObj.companyRole)}
+              name={itemObj.name}
+            />
+            <div style={{ fontWeight: 600 }}>{item}</div>
+          </div>
+        );
+      },
+    },
+    {
+      title: 'TAX ID',
+      dataIndex: 'taxId',
+      key: 'taxId',
+      align: 'left' as const,
+    },
+    {
+      title: 'COMPANY TYPE',
+      dataIndex: 'companyRole',
+      key: 'companyRole',
+      align: 'left' as const,
+      render: (item: any) => {
+        return getCompanyRoleComponent(item);
+      },
+    },
+    {
+      title: '',
+      width: 6,
+      align: 'right' as const,
+      render: (_: any, record: TableDataType) => {
+        return (
+          <Popover placement="bottomRight" content={actionMenu(record)} trigger="click">
+            <EllipsisOutlined
+              rotate={90}
+              style={{ fontWeight: 600, fontSize: '1rem', cursor: 'pointer' }}
+            />
+          </Popover>
+        );
+      },
+    },
+  ];
+  // }
+
+  const getAllCompany = async () => {
+    setLoading(true);
+    try {
+      const response: any = await get('company/query', {
+        params: {
+          page: currentPage,
+          size: pageSize,
+        },
+      });
+      setTableData(response.data);
+      setTotalCompany(response.response.data.total);
+      setLoading(false);
+    } catch (error: any) {
+      console.log('Error in getting company', error);
+      message.open({
+        type: 'error',
+        content: error.message,
+        duration: 3,
+        style: { textAlign: 'right', marginRight: 15, marginTop: 10 },
+      });
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getAllCompany();
+  }, [currentPage, pageSize]);
+
+  const onChange: PaginationProps['onChange'] = (page, size) => {
+    setCurrentPage(page);
+    setPageSize(size);
+  };
+
+  return (
+    <div className="content-container">
+      <div className="title-bar">
+        <div className="body-title">View Companies</div>
+        <div className="body-sub-title">
+          View all the visible companies in the system based on your permissions
+        </div>
+      </div>
+      <div className="content-card">
+        <Row>
+          <div className="action-bar">
+            <Button
+              type="primary"
+              size="large"
+              block
+              icon={<PlusOutlined />}
+              onClick={() => navigate('/userManagement/addUser')}
+            >
+              Add Company
+            </Button>
+          </div>
+        </Row>
+        <Row>
+          <Col span={24}>
+            <div className="userManagement-table-container">
+              <Table
+                dataSource={tableData}
+                columns={columns}
+                className="common-table-class"
+                loading={loading}
+                pagination={{
+                  current: currentPage,
+                  pageSize: pageSize,
+                  total: totalCompany,
+                  showQuickJumper: true,
+                  showSizeChanger: true,
+                  onChange: onChange,
+                }}
+                // scroll={{ x: 1500 }}
+                locale={{
+                  emptyText: (
+                    <Empty
+                      image={Empty.PRESENTED_IMAGE_SIMPLE}
+                      description={tableData.length === 0 ? 'No Companies' : null}
+                    />
+                  ),
+                }}
+              />
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+};
+
+export default CompanyManagement;
