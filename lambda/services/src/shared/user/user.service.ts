02ba79e1256eaf2d82f0b7df5121f8f6334a2341
Yathurshan Thurairajah <yathurshant@xeptagon.com>
2022-12-30 11:31:12 +0530

    Fixed unreasolved conflicts issues

--- lambda/services/src/shared/user/user.service.ts
+++ lambda/services/src/shared/user/user.service.ts
@@ -79,6 +79,7 @@ export class UserService {
         "apiKey",
         "companyId",
         "companyRole",
+        "name",
       ],
       where: {
         email: username,
@@ -113,7 +114,14 @@ export class UserService {
       .createQueryBuilder()
       .update(User)
       .set(update)
-      .where(`id = ${id} ${abilityCondition ? " AND " + abilityCondition : ""}`)
+      .where(
+        `id = ${id} ${
+          abilityCondition
+            ? " AND " +
+              this.helperService.parseMongoQueryToSQL(abilityCondition)
+            : ""
+        }`
+      )
       .execute()
       .catch((err: any) => {
         this.logger.error(err);
@@ -135,7 +143,12 @@ export class UserService {
     const user = await this.userRepo
       .createQueryBuilder()
       .where(
-        `id = '${id}' ${abilityCondition ? " AND " + abilityCondition : ""}`
+        `id = '${id}' ${
+          abilityCondition
+            ? " AND " +
+              this.helperService.parseMongoQueryToSQL(abilityCondition)
+            : ""
+        }`
       )
       .addSelect(["User.password"])
       .getOne();
@@ -170,7 +183,10 @@ export class UserService {
       .createQueryBuilder()
       .where(
         `email = '${email}' ${
-          abilityCondition ? " AND " + abilityCondition : ""
+          abilityCondition
+            ? " AND " +
+              this.helperService.parseMongoQueryToSQL(abilityCondition)
+            : ""
         }`
       )
       .getOne();
@@ -224,214 +240,12 @@ export class UserService {
       );
     }
 
-    async getUserCredentials(username: string): Promise<User | undefined> {
-        const users = await this.userRepo.find({
-            select: ['id', 'email', 'password', 'role', 'apiKey', 'companyId', 'companyRole', 'name'],
-            where: {
-                email: username,
-            }
-        });
-        return (users && users.length > 0) ? users[0] : undefined;
-    }
-
-    async findOne(username: string): Promise<User | undefined> {
-        const users = await this.userRepo.find({
-            where: {
-                email: username,
-            }
-        });
-        return (users && users.length > 0) ? users[0] : undefined;
-    }
-
-    async findById(id: number): Promise<User | undefined> {
-        return await this.userRepo.findOneBy({
-            id: id,
-        });
-    }
-
-    async update(userDto: UserUpdateDto, abilityCondition: string): Promise<DataResponseDto | undefined> {
-        this.logger.verbose('User update received', abilityCondition)
-        const { id, ...update } = userDto;
-
-        const result = await this.userRepo.createQueryBuilder()
-            .update(User)
-            .set(update)
-            .where(`id = ${id} ${abilityCondition ? (' AND ' + this.helperService.parseMongoQueryToSQL(abilityCondition)) : ""}`)
-            .execute().catch((err: any) => {
-                this.logger.error(err)
-                return err;
-            });
-        if (result.affected) {
-            return new DataResponseDto(HttpStatus.OK, await this.findById(id));
-        }
-        throw new HttpException("No visible user found", HttpStatus.NOT_FOUND)
-    }
-
-    async resetPassword(id: number, passwordResetDto: PasswordUpdateDto, abilityCondition: string) {
-        this.logger.verbose('User password reset received', id)
-
-        const user = await this.userRepo.createQueryBuilder().where(`id = '${id}' ${abilityCondition ? ' AND ' + this.helperService.parseMongoQueryToSQL(abilityCondition) : ""}`).addSelect(["User.password"]).getOne()
-        if (!user || user.password != passwordResetDto.oldPassword) {
-            throw new HttpException("Password mismatched", HttpStatus.UNAUTHORIZED)
-        }
-        const result = await this.userRepo.update({
-            id: id
-        }, {
-            password: passwordResetDto.newPassword
-        }).catch((err: any) => {
-            this.logger.error(err)
-            return err;
-        });
-        if (result.affected > 0) {
-            return new BasicResponseDto(HttpStatus.OK, "Successfully updated");
-        }
-        throw new HttpException("Password update failed. Please try again", HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    async regenerateApiKey(email, abilityCondition) {
-        this.logger.verbose('Regenerated api key received', email)
-        const user = await this.userRepo.createQueryBuilder().where(`email = '${email}' ${abilityCondition ? ' AND ' + this.helperService.parseMongoQueryToSQL(abilityCondition) : ""}`).getOne()
-        if (!user) {
-            throw new HttpException("No visible user found", HttpStatus.UNAUTHORIZED)
-        }
-        const apiKey = await this.generateApiKey(email)
-        const result = await this.userRepo.update({
-            id: user.id
-        }, {
-            apiKey: apiKey
-        }).catch((err: any) => {
-            this.logger.error(err)
-            return err;
-        });
-
-        if (result.affected > 0) {
-            await this.emailService.sendEmail(
-                user.email,
-                EmailTemplates.API_KEY_EMAIL,
-                {
-                    "name": user.name,
-                    "apiKey": apiKey
-                });
-
-            return new BasicResponseDto(HttpStatus.OK, "Successfully updated");
-        }
-        throw new HttpException("Password update failed. Please try again", HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-    async create(userDto: UserDto, companyId: number, companyRole: CompanyRole): Promise<User | undefined> {
-        this.logger.verbose(`User create received  ${userDto.email} ${companyId}`)
-
-        const user = await this.findOne(userDto.email);
-        if (user) {
-            throw new HttpException("User already exist in the system", HttpStatus.BAD_REQUEST);
-        }
-
-        let { company, ...userFields } = userDto
-        if (company) {
-
-            if (userFields.role && userFields.role != Role.Admin) {
-                throw new HttpException("Company create user should be an Admin user", HttpStatus.BAD_REQUEST)
-            } else if (!userFields.role) {
-                userFields.role = Role.Admin;
-            }
-
-            if (company.companyRole != CompanyRole.CERTIFIER || !company.country) {
-                company.country = this.configService.get('systemCountry')
-            }
-            
-            if (company.companyRole == CompanyRole.GOVERNMENT) {
-                const companyGov = await this.companyService.findGovByCountry(company.country);
-                if (companyGov) {
-                    throw new HttpException(`Government already exist for the country code ${company.country}`, HttpStatus.BAD_REQUEST);
-                }
-            }
-            
-            if (companyRole != CompanyRole.GOVERNMENT) {
-                throw new HttpException("Company create does not permitted for your company role", HttpStatus.FORBIDDEN)
-            }
-        }
-
-        if (CompanyRole.GOVERNMENT != companyRole && userDto.companyId && userDto.companyId != companyId) {
-            throw new HttpException("Not authorized to add users to other companies", HttpStatus.FORBIDDEN)
-        }
-        
-        const u: User = plainToClass(User, userFields);
-        if (userDto.company) {
-            u.companyRole = userDto.company.companyRole
-        } else if (u.companyId) {
-            const company = await this.companyService.findByCompanyId(u.companyId);
-            if (!company) {
-                throw new HttpException("Invalid programme id", HttpStatus.BAD_REQUEST)
-            }
-            u.companyRole = company.companyRole
-        } else {
-            u.companyId = companyId
-            u.companyRole = companyRole
-        }
-
-        if (u.companyRole != CompanyRole.CERTIFIER || !u.country) {
-            u.country = this.configService.get('systemCountry');
-        }
-
-        u.password = this.generateRandomPassword()   
-        if (userDto.role == Role.Admin && u.companyRole == CompanyRole.MRV) {
-            u.apiKey = await this.generateApiKey(userDto.email)
-        }
-
-        await this.emailService.sendEmail(
-            u.email,
-            EmailTemplates.REGISTER_EMAIL,
-            {
-                "name": u.name,
-                "countryName": this.configService.get('systemCountry'),
-                "password": u.password,
-                "apiKeyText":u.apiKey? `<br>Api Key: ${u.apiKey}`: ""
-            });
-        
-        const usr = await this.entityManger.transaction(async (em) => {
-            if (company) {
-                const c: Company = await em.save<Company>(plainToClass(Company, company));
-                u.companyId = c.companyId
-                u.companyRole = company.companyRole
-            }
-            const user = await em.save<User>(u);
-            return user;
-        }).catch((err: any) => {
-            console.log(err)
-            if (err instanceof QueryFailedError) {
-                console.log(err)
-                switch (err.driverError.code) {
-                    case PG_UNIQUE_VIOLATION:
-                        if (err.driverError.detail.includes('email')) {
-                            throw new HttpException(`${err.driverError.table == 'company' ? 'Company email' : 'Email'} already exist`, HttpStatus.BAD_REQUEST);
-                        } else if (err.driverError.detail.includes('taxId')) {
-                            throw new HttpException('Company tax id already exist', HttpStatus.BAD_REQUEST);
-                        } 
-                }
-                this.logger.error(`User add error ${err}`)
-            } else {
-                this.logger.error(`User add error ${err}`)
-            }
-            return err;
-        })
-
-        const { apiKey, password, ...resp } = usr
-        return resp;
-    }
-
-    async query(query: QueryDto, abilityCondition: string): Promise<any> {
-        console.log("query ----> ", this.helperService.generateWhereSQL(query, abilityCondition, '"user"'))
-
-        const resp = (await this.userRepo.createQueryBuilder('user')
-            .where(this.helperService.generateWhereSQL(query, this.helperService.parseMongoQueryToSQLWithTable('"user"', abilityCondition), '"user"'))
-            // .leftJoinAndSelect("user.companyId", "company")
-            .skip((query.size * query.page) - query.size)
-            .take(query.size)
-            .leftJoinAndMapOne('user.company', Company, 'company', 'company.companyId = user.companyId')
-            .getManyAndCount())
-
-        return new DataListResponseDto(
-            resp.length > 0 ? resp[0] : undefined,
-            resp.length > 1 ? resp[1] : undefined
+    let { company, ...userFields } = userDto;
+    if (company) {
+      if (userFields.role && userFields.role != Role.Admin) {
+        throw new HttpException(
+          "Company create user should be an Admin user",
+          HttpStatus.BAD_REQUEST
         );
       } else if (!userFields.role) {
         userFields.role = Role.Admin;
@@ -548,31 +362,22 @@ export class UserService {
   }
 
   async query(query: QueryDto, abilityCondition: string): Promise<any> {
-    const sql = await this.userRepo
-      .createQueryBuilder("user")
-      .where(
-        this.helperService.generateWhereSQL(query, abilityCondition, '"user"')
-      )
-      .orderBy(
-        query?.sort?.key && `"user"."${query?.sort?.key}"`,
-        query?.sort?.order
-      )
-      // .leftJoinAndSelect("user.companyId", "company")
-      .skip(query.size * query.page - query.size)
-      .take(query.size)
-      .leftJoinAndMapOne(
-        "user.company",
-        Company,
-        "company",
-        "company.companyId = user.companyId"
-      )
-      .getSql();
+    console.log(
+      "query ----> ",
+      this.helperService.generateWhereSQL(query, abilityCondition, '"user"')
+    );
 
-    console.log("SQL query -> ", sql);
     const resp = await this.userRepo
       .createQueryBuilder("user")
       .where(
-        this.helperService.generateWhereSQL(query, abilityCondition, '"user"')
+        this.helperService.generateWhereSQL(
+          query,
+          this.helperService.parseMongoQueryToSQLWithTable(
+            '"user"',
+            abilityCondition
+          ),
+          '"user"'
+        )
       )
       .orderBy(
         query?.sort?.key && `"user"."${query?.sort?.key}"`,
@@ -600,7 +405,13 @@ export class UserService {
 
     const result = await this.userRepo
       .createQueryBuilder()
-      .where(`email = '${username}' ${ability ? " AND " + ability : ""}`)
+      .where(
+        `email = '${username}' ${
+          ability
+            ? " AND " + this.helperService.parseMongoQueryToSQL(ability)
+            : ""
+        }`
+      )
       .getMany();
     if (result.length <= 0) {
       throw new HttpException("No visible user found", HttpStatus.NOT_FOUND);
--- lambda/services/src/shared/util/helpers.service.ts
+++ lambda/services/src/shared/util/helpers.service.ts
@@ -4,42 +4,41 @@ import { QueryDto } from "../dto/query.dto";
 
 @Injectable()
 export class HelperService {
-  private prepareValue(value: any) {
-    console.log(value.constructor);
-    if (value instanceof Array) {
-      return "(" + value.map((e) => `'${e}'`).join(",") + ")";
-    } else if (typeof value === "string") {
-      return "'" + value + "'";
+
+    private prepareValue(value: any) {
+        console.log(value.constructor)
+        if (value instanceof Array) {
+            return '(' + value.map((e) => `'${e}'`).join(',') + ')'
+        } else if (typeof value === 'string') {
+            return "'" + value + "'"
+        }
+        return value;
     }
-    return value;
-  }
-  public generateWhereSQL(query: QueryDto, extraSQL: string, table?: string) {
-    let sql = "";
-    if (query.filterAnd) {
-      sql += query.filterAnd
-        .map(
-          (e) =>
-            `${table ? table + "." : ""}"${e.key}" ${
-              e.operation
-            } ${this.prepareValue(e.value)}`
-        )
-        .join(" and ");
+    public generateWhereSQL(query: QueryDto, extraSQL: string, table?: string) {
+        let sql = '';
+        if (query.filterAnd) {
+            sql += query.filterAnd.map((e) => `${table ? table + '.' : ''}"${e.key}" ${e.operation} ${this.prepareValue(e.value)}`).join(' and ')
+        }
+        if (query.filterOr) {
+            const orSQl = query.filterOr.map((e) => `${table ? table + '.' : ''}"${e.key}" ${e.operation} ${typeof e.value === 'string' ? "'" + e.value + "'" : e.value}`).join(' or ')
+            if (sql != '') {
+                sql = `(${sql}) and (${orSQl})`
+            } else {
+                sql = orSQl
+            }
+        }
+
+        if (sql != '') {
+            if (extraSQL) {
+                sql = `(${sql}) and (${extraSQL})`
+            }
+        } else if (extraSQL) {
+            sql = extraSQL;
+        }
+        console.log(sql)
+
+        return sql;
     }
-<<<<<<< HEAD
-    if (query.filterOr) {
-      const orSQl = query.filterOr
-        .map(
-          (e) =>
-            `${table ? table + "." : ""}"${e.key}" ${e.operation} ${
-              typeof e.value === "string" ? "'" + e.value + "'" : e.value
-            }`
-        )
-        .join(" or ");
-      if (sql != "") {
-        sql = `(${sql}) and (${orSQl})`;
-      } else {
-        sql = orSQl;
-=======
 
     public parseMongoQueryToSQL(mongoQuery, isNot = false, key = undefined) {
         return this.parseMongoQueryToSQLWithTable(undefined, mongoQuery, isNot, key)
@@ -71,76 +70,5 @@ export class HelperService {
           }
         }
         return final;
->>>>>>> 507fe3e12af72613873e427989d9a9f5f9794eb3
       }
-    }
-
-    if (sql != "") {
-      if (extraSQL) {
-        sql = `(${sql}) and (${extraSQL})`;
-      }
-    } else if (extraSQL) {
-      sql = extraSQL;
-    }
-
-    return sql;
-  }
-
-  public parseMongoQueryToSQL(mongoQuery, isNot = false, key = undefined) {
-    return this.parseMongoQueryToSQLWithTable(
-      undefined,
-      mongoQuery,
-      isNot,
-      key
-    );
-  }
-
-  public parseMongoQueryToSQLWithTable(
-    table,
-    mongoQuery,
-    isNot = false,
-    key = undefined
-  ) {
-    let final = undefined;
-    for (let operator in mongoQuery) {
-      if (operator.startsWith("$")) {
-        if (operator == "$and" || operator == "$or") {
-          const val = mongoQuery[operator]
-            .map((st) => this.parseMongoQueryToSQLWithTable(table, st))
-            .join(` ${operator.replace("$", "")} `);
-          final = final == undefined ? val : `${final} and ${val}`;
-        } else if (operator == "$not") {
-          return this.parseMongoQueryToSQLWithTable(
-            table,
-            mongoQuery["$not"],
-            !isNot
-          );
-        } else if (operator == "$eq") {
-          const value =
-            typeof mongoQuery["$eq"] === "number"
-              ? String(mongoQuery["$eq"])
-              : `'${mongoQuery["$eq"]}'`;
-          return `${table ? table + "." : ""}"${key}" ${
-            isNot ? "!=" : "="
-          } ${value}`;
-        } else if (operator == "$ne") {
-          const value =
-            typeof mongoQuery["$ne"] === "number"
-              ? String(mongoQuery["$ne"])
-              : `'${mongoQuery["$ne"]}'`;
-          return `${table ? table + "." : ""}"${key}" ${
-            isNot ? "=" : "!="
-          } ${value}`;
-        }
-      } else {
-        return this.parseMongoQueryToSQLWithTable(
-          table,
-          mongoQuery[operator],
-          isNot,
-          operator
-        );
-      }
-    }
-    return final;
-  }
 }
