9f639d74d79c477aeaa63e35da5c3719f8605ba6
Yathurshan Thurairajah <yathurshant@xeptagon.com>
2023-01-20 09:05:17 +0530

    Divided dashboard chart stats codebase into components

---
+++ web/src/Pages/Dashboard/barChartStats.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from 'react';
+import { DatePicker, Progress, Skeleton } from 'antd';
+import Chart from 'react-apexcharts';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import fileText from '../../Assets/Images/fileText.svg';
+import { CarOutlined, CloseCircleOutlined } from '@ant-design/icons';
+import moment from 'moment';
+import { ClockHistory, HandThumbsUp, XCircle, Clipboard2Pulse } from 'react-bootstrap-icons';
+
+const { RangePicker } = DatePicker;
+
+export interface BarChartStatsProps {
+  title: string;
+  options: any;
+  series: any;
+  lastUpdate: number;
+  loading: boolean;
+}
+
+const BarChartsStat: FC<BarChartStatsProps> = (props: BarChartStatsProps) => {
+  const { title, options, series, lastUpdate, loading } = props;
+  return (
+    <div className="stastics-and-pie-card height-bar-rem">
+      <div className="pie-charts-title">{title}</div>
+      {loading ? (
+        <div className="margin-top-2">
+          <Skeleton active />
+          <Skeleton active />
+        </div>
+      ) : (
+        <>
+          <div className="pie-charts-section">
+            <Chart options={options} series={series} type="bar" height="350px" width="490px" />
+          </div>
+          <div className="updated-on">
+            <div className="updated-moment-container">{moment(lastUpdate * 1000).fromNow()}</div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default BarChartsStat;
--- web/src/Pages/Dashboard/dashboard.tsx
+++ web/src/Pages/Dashboard/dashboard.tsx
@@ -29,6 +29,8 @@ import {
   ShieldCheck,
   Gem,
 } from 'react-bootstrap-icons';
+import PieChartsStat from './pieChartStat';
+import BarChartsStat from './barChartStats';
 
 const { RangePicker } = DatePicker;
 
@@ -38,6 +40,7 @@ mapboxgl.accessToken =
 const Dashboard = () => {
   const { get, post, delete: del } = useConnection();
   const mapContainerRef = useRef(null);
+  const mapContainerInternationalRef = useRef(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [userDetails, setUserDetails] = useState<any>([]);
   const [companyRole, setCompanyRole] = useState<any>();
@@ -672,6 +675,185 @@ const Dashboard = () => {
     }, 1000);
   }, [programmeLocations]);
 
+  useEffect(() => {
+    const address = programmeLocations[0];
+    // filters for classifying earthquakes into five categories based on magnitude
+    const mag1 = ['<', ['get', 'mag'], 2];
+    const mag2 = ['all', ['>=', ['get', 'mag'], 2], ['<', ['get', 'mag'], 3]];
+    const mag3 = ['all', ['>=', ['get', 'mag'], 3], ['<', ['get', 'mag'], 4]];
+    const mag4 = ['all', ['>=', ['get', 'mag'], 4], ['<', ['get', 'mag'], 5]];
+    const mag5 = ['>=', ['get', 'mag'], 5];
+
+    // colors to use for the categories
+    const colors = ['#fed976', '#feb24c', '#fd8d3c', '#fc4e2a', '#e31a1c'];
+
+    function donutSegment(start: any, end: any, r: any, r0: any, color: any) {
+      if (end - start === 1) end -= 0.00001;
+      const a0 = 2 * Math.PI * (start - 0.25);
+      const a1 = 2 * Math.PI * (end - 0.25);
+      const x0 = Math.cos(a0),
+        y0 = Math.sin(a0);
+      const x1 = Math.cos(a1),
+        y1 = Math.sin(a1);
+      const largeArc = end - start > 0.5 ? 1 : 0;
+
+      // draw an SVG path
+      return `<path d="M ${r + r0 * x0} ${r + r0 * y0} L ${r + r * x0} ${
+        r + r * y0
+      } A ${r} ${r} 0 ${largeArc} 1 ${r + r * x1} ${r + r * y1} L ${r + r0 * x1} ${
+        r + r0 * y1
+      } A ${r0} ${r0} 0 ${largeArc} 0 ${r + r0 * x0} ${r + r0 * y0}" fill="${color}" />`;
+    }
+
+    // code for creating an SVG donut chart from feature properties
+    function createDonutChart(props: any) {
+      const offsets = [];
+      const counts = [props.mag1, props.mag2, props.mag3, props.mag4, props.mag5];
+      let total = 0;
+      for (const count of counts) {
+        offsets.push(total);
+        total += count;
+      }
+      const fontSize = total >= 1000 ? 22 : total >= 100 ? 20 : total >= 10 ? 18 : 16;
+      const r = total >= 1000 ? 50 : total >= 100 ? 32 : total >= 10 ? 24 : 18;
+      const r0 = Math.round(r * 0.6);
+      const w = r * 2;
+
+      let html = `<div>
+  <svg width="${w}" height="${w}" viewbox="0 0 ${w} ${w}" text-anchor="middle" style="font: ${fontSize}px sans-serif; display: block">`;
+
+      for (let i = 0; i < counts.length; i++) {
+        html += donutSegment(
+          offsets[i] / total,
+          (offsets[i] + counts[i]) / total,
+          r,
+          r0,
+          colors[i]
+        );
+      }
+      html += `<circle cx="${r}" cy="${r}" r="${r0}" fill="white" />
+  <text dominant-baseline="central" transform="translate(${r}, ${r})">
+  ${total.toLocaleString()}
+  </text>
+  </svg>
+  </div>`;
+
+      const el = document.createElement('div');
+      el.innerHTML = html;
+      return el.firstChild;
+    }
+
+    setTimeout(() => {
+      if (mapContainerInternationalRef.current) {
+        const map = new mapboxgl.Map({
+          container: mapContainerInternationalRef.current || '',
+          zoom: 0.5,
+          center: [0, 20],
+          // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+          style: 'mapbox://styles/mapbox/light-v11',
+        });
+        map.on('load', () => {
+          // add a clustered GeoJSON source for a sample set of earthquakes
+          map.addSource('earthquakes', {
+            type: 'geojson',
+            data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+            cluster: true,
+            clusterRadius: 80,
+            clusterProperties: {
+              // keep separate counts for each magnitude category in a cluster
+              mag1: ['+', ['case', mag1, 1, 0]],
+              mag2: ['+', ['case', mag2, 1, 0]],
+              mag3: ['+', ['case', mag3, 1, 0]],
+              mag4: ['+', ['case', mag4, 1, 0]],
+              mag5: ['+', ['case', mag5, 1, 0]],
+            },
+          });
+          // circle and symbol layers for rendering individual earthquakes (unclustered points)
+          map.addLayer({
+            id: 'earthquake_circle',
+            type: 'circle',
+            source: 'earthquakes',
+            filter: ['!=', 'cluster', true],
+            paint: {
+              'circle-color': [
+                'case',
+                mag1,
+                colors[0],
+                mag2,
+                colors[1],
+                mag3,
+                colors[2],
+                mag4,
+                colors[3],
+                colors[4],
+              ],
+              'circle-opacity': 0.6,
+              'circle-radius': 12,
+            },
+          });
+          map.addLayer({
+            id: 'earthquake_label',
+            type: 'symbol',
+            source: 'earthquakes',
+            filter: ['!=', 'cluster', true],
+            layout: {
+              'text-field': [
+                'number-format',
+                ['get', 'mag'],
+                { 'min-fraction-digits': 1, 'max-fraction-digits': 1 },
+              ],
+              'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+              'text-size': 10,
+            },
+            paint: {
+              'text-color': ['case', ['<', ['get', 'mag'], 3], 'black', 'white'],
+            },
+          });
+
+          // objects for caching and keeping track of HTML marker objects (for performance)
+          const markers: any = {};
+          let markersOnScreen: any = {};
+
+          function updateMarkers() {
+            const newMarkers: any = {};
+            const features: any = map.querySourceFeatures('earthquakes');
+
+            // for every cluster on the screen, create an HTML marker for it (if we didn't yet),
+            // and add it to the map if it's not there already
+            for (const feature of features) {
+              const coords = feature.geometry.coordinates;
+              const props = feature.properties;
+              if (!props.cluster) continue;
+              const id = props.cluster_id;
+
+              let marker: any = markers[id];
+              if (!marker) {
+                const el: any = createDonutChart(props);
+                marker = markers[id] = new mapboxgl.Marker({
+                  element: el,
+                }).setLngLat(coords);
+              }
+              newMarkers[id] = marker;
+
+              if (!markersOnScreen[id]) marker.addTo(map);
+            }
+            // for every marker we've added previously, remove those that are no longer visible
+            for (const id in markersOnScreen) {
+              if (!newMarkers[id]) markersOnScreen[id].remove();
+            }
+            markersOnScreen = newMarkers;
+          }
+
+          // after the GeoJSON data is loaded, update markers on the screen on every frame
+          map.on('render', () => {
+            if (!map.isSourceLoaded('earthquakes')) return;
+            updateMarkers();
+          });
+        });
+      }
+    }, 1000);
+  }, [programmeLocations]);
+
   return (
     <div className="dashboard-main-container">
       <div className="stastics-cards-container">
@@ -806,178 +988,66 @@ const Dashboard = () => {
             />
           </Col>
           <Col xxl={8} xl={8} md={12} className="stastic-card-col">
-            <div className="stastics-and-pie-card height-pie-rem">
-              {loading ? (
-                <div className="margin-top-2">
-                  <Skeleton active />
-                  <Skeleton active />
-                </div>
-              ) : (
-                <>
-                  <div className="pie-charts-title">Credits</div>
-                  <div className="pie-charts-section">
-                    <Chart
-                      options={optionDonutPieA}
-                      series={creditsPieSeries}
-                      type="donut"
-                      width="350px"
-                    />
-                  </div>
-                  <div className="updated-on margin-top-2">
-                    <div className="updated-moment-container">
-                      {moment(lastUpdate * 1000).fromNow()}
-                    </div>
-                  </div>
-                </>
-              )}
-            </div>
+            <PieChartsStat
+              title="Credits"
+              options={optionDonutPieA}
+              series={creditsPieSeries}
+              lastUpdate={lastUpdate}
+              loading={loading}
+            />
           </Col>
           <Col xxl={8} xl={8} md={12} className="stastic-card-col">
-            <div className="stastics-and-pie-card height-pie-rem">
-              {loading ? (
-                <div className="margin-top-2">
-                  <Skeleton active />
-                  <Skeleton active />
-                </div>
-              ) : (
-                <>
-                  <div className="pie-charts-title">Certified Credits</div>
-                  <div className="pie-charts-section">
-                    <Chart
-                      options={optionDonutPieB}
-                      series={creditsCertifiedPieSeries}
-                      type="donut"
-                      width="350px"
-                    />
-                  </div>
-                  <div className="updated-on margin-top-2">
-                    <div className="updated-moment-container">
-                      {moment(lastUpdate * 1000).fromNow()}
-                    </div>
-                  </div>
-                </>
-              )}
-            </div>
+            <PieChartsStat
+              title="Certified Credits"
+              options={optionDonutPieB}
+              series={creditsCertifiedPieSeries}
+              lastUpdate={lastUpdate}
+              loading={loading}
+            />
           </Col>
         </Row>
       </div>
       <div className="stastics-and-charts-container center">
         <Row gutter={[40, 40]} className="stastic-card-row">
           <Col xxl={12} xl={12} md={12} className="stastic-card-col">
-            <div className="stastics-and-pie-card height-bar-rem">
-              <div className="pie-charts-title">Total Programmes</div>
-              {loading ? (
-                <div className="margin-top-2">
-                  <Skeleton active />
-                  <Skeleton active />
-                </div>
-              ) : (
-                <>
-                  <div className="pie-charts-section">
-                    <Chart
-                      options={totalProgrammesOptions}
-                      series={seriesTotalProgrammesY}
-                      type="bar"
-                      height="350px"
-                      width="490px"
-                    />
-                  </div>
-                  <div className="updated-on">
-                    <div className="updated-moment-container">
-                      {moment(lastUpdate * 1000).fromNow()}
-                    </div>
-                  </div>
-                </>
-              )}
-            </div>
+            <BarChartsStat
+              title="Total Programmes"
+              options={totalProgrammesOptions}
+              series={seriesTotalProgrammesY}
+              lastUpdate={lastUpdate}
+              loading={loading}
+            />
           </Col>
           <Col xxl={12} xl={12} md={12} className="stastic-card-col">
-            <div className="stastics-and-pie-card height-bar-rem">
-              <div className="pie-charts-title">Total Programmes:Sector</div>
-              {loading ? (
-                <div className="margin-top-2">
-                  <Skeleton active />
-                  <Skeleton active />
-                </div>
-              ) : (
-                <>
-                  <div className="pie-charts-section">
-                    <Chart
-                      options={totalProgrammesOptionsSub}
-                      series={seriesTotalProgrammesSubY}
-                      type="bar"
-                      height="350px"
-                      width="490px"
-                    />
-                  </div>
-                  <div className="updated-on">
-                    <div className="updated-moment-container">
-                      {moment(lastUpdate * 1000).fromNow()}
-                    </div>
-                  </div>
-                </>
-              )}
-            </div>
+            <BarChartsStat
+              title="Total Programmes:Sector"
+              options={totalProgrammesOptionsSub}
+              series={seriesTotalProgrammesSubY}
+              lastUpdate={lastUpdate}
+              loading={loading}
+            />
           </Col>
         </Row>
       </div>
       <div className="stastics-and-charts-container center">
         <Row gutter={[40, 40]} className="stastic-card-row">
           <Col xxl={12} xl={12} md={12} className="stastic-card-col">
-            <div className="stastics-and-pie-card height-bar-rem">
-              <div className="pie-charts-title">Total Credits</div>
-              {loading ? (
-                <div className="margin-top-2">
-                  <Skeleton active />
-                  <Skeleton active />
-                </div>
-              ) : (
-                <>
-                  <div className="pie-charts-section">
-                    <Chart
-                      options={totalCreditsOptions}
-                      series={seriesTotalCreditsY}
-                      type="bar"
-                      height="350px"
-                      width="490px"
-                    />
-                  </div>
-                  <div className="updated-on">
-                    <div className="updated-moment-container">
-                      {moment(lastUpdate * 1000).fromNow()}
-                    </div>
-                  </div>
-                </>
-              )}
-            </div>
+            <BarChartsStat
+              title="Total Credits"
+              options={totalCreditsOptions}
+              series={seriesTotalCreditsY}
+              lastUpdate={lastUpdate}
+              loading={loading}
+            />
           </Col>
           <Col xxl={12} xl={12} md={12} className="stastic-card-col">
-            <div className="stastics-and-pie-card height-bar-rem">
-              <div className="pie-charts-title">Total Credit Certified</div>
-              {loading ? (
-                <div className="margin-top-2">
-                  <Skeleton active />
-                  <Skeleton active />
-                </div>
-              ) : (
-                <>
-                  <div className="pie-charts-section">
-                    <Chart
-                      options={totalProgrammesOptions}
-                      series={seriesY}
-                      type="bar"
-                      height="350px"
-                      width="450px"
-                    />
-                  </div>
-                  <div className="updated-on">
-                    <div className="updated-moment-container">
-                      {moment(lastUpdate * 1000).fromNow()}
-                    </div>
-                  </div>
-                </>
-              )}
-            </div>
+            <BarChartsStat
+              title="Total Credit Certified"
+              options={totalProgrammesOptions}
+              series={seriesY}
+              lastUpdate={lastUpdate}
+              loading={loading}
+            />
           </Col>
         </Row>
       </div>
@@ -1005,7 +1075,7 @@ const Dashboard = () => {
               )}
             </div>
           </Col>
-          {/* <Col xxl={12} xl={12} md={12} className="stastic-card-col">
+          <Col xxl={12} xl={12} md={12} className="stastic-card-col">
             <div className="stastics-and-pie-card height-map-rem">
               <div className="pie-charts-title">Transfer Locations International</div>
               {loading ? (
@@ -1016,7 +1086,7 @@ const Dashboard = () => {
               ) : (
                 <>
                   <div className="map-content">
-                    <div className="map-container" ref={mapContainerRef} />
+                    <div className="map-container" ref={mapContainerInternationalRef} />
                   </div>
                   <div className="updated-on">
                     <div className="updated-moment-container">
@@ -1026,7 +1096,7 @@ const Dashboard = () => {
                 </>
               )}
             </div>
-          </Col> */}
+          </Col>
         </Row>
       </div>
     </div>
