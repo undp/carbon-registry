5d135b8bf61df93369d83db2e545df4932180ce3
dhanushkaxep <dhanushkap@xeptagon.com>
2023-02-03 16:56:32 +0530

    add un member countries

--- web/src/Pages/Company/addNewCompany.tsx
+++ web/src/Pages/Company/addNewCompany.tsx
@@ -29,15 +29,27 @@ const AddNewCompany = () => {
   const [current, setCurrent] = useState<number>(0);
   const [isUpdate, setIsUpdate] = useState(false);
   const { i18n, t } = useTranslation(['addCompany']);
-  const { put } = useConnection();
+  const { put, get } = useConnection();
   const [fileList, setFileList] = useState<UploadFile[]>([]);
   const maximumImageSize = process.env.MAXIMUM_IMAGE_SIZE
     ? parseInt(process.env.MAXIMUM_IMAGE_SIZE)
     : 3145728;
   const { setUserInfo } = useUserContext();
+  const [countries, setCountries] = useState<[]>([]);
+
+  const getCountryList = async () => {
+    const response = await get('national/organisation/countries');
+    if (response.data) {
+      const alpha2Names = response.data.map((item: any) => {
+        return item.alpha2;
+      });
+      setCountries(alpha2Names);
+    }
+  };
 
   useEffect(() => {
     setIsUpdate(state?.record ? true : false);
+    getCountryList();
     if (state?.record?.logo) {
       setFileList([
         {
@@ -433,6 +445,7 @@ const AddNewCompany = () => {
                       defaultCountry="LK"
                       countryCallingCodeEditable={false}
                       onChange={(v) => {}}
+                      countries={countries}
                     />
                   </Form.Item>
                   <Form.Item
