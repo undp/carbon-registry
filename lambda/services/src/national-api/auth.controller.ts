971f2d52173bf7cf95690ff5ac71a509c349bc57
Yathurshan Thurairajah <yathurshant@xeptagon.com>
2023-02-27 12:27:02 +0530

    added json files regarding the success and error messages in the backend for english

--- lambda/services/src/national-api/auth.controller.ts
+++ lambda/services/src/national-api/auth.controller.ts
@@ -1,24 +1,35 @@
-import { Controller, Get, Post, UseGuards, Request, Logger, Body, ValidationPipe, UnauthorizedException } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { LoginDto } from '../shared/dto/login.dto';
-import { AuthService } from '../shared/auth/auth.service';
-import { JwtAuthGuard } from '../shared/auth/guards/jwt-auth.guard';
-import { LocalAuthGuard } from '../shared/auth/guards/local-auth.guard';
+import {
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+  Logger,
+  Body,
+  ValidationPipe,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+import { I18n, I18nContext } from "nestjs-i18n";
+import { LoginDto } from "../shared/dto/login.dto";
+import { AuthService } from "../shared/auth/auth.service";
+import { JwtAuthGuard } from "../shared/auth/guards/jwt-auth.guard";
+import { LocalAuthGuard } from "../shared/auth/guards/local-auth.guard";
 
-@ApiTags('Auth')
-@Controller('auth')
+@ApiTags("Auth")
+@Controller("auth")
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
 
-    constructor(private readonly authService: AuthService) {}
-
-    @Post('login')
-    async login(@Body() login: LoginDto) {
-      const user = await this.authService.validateUser(login.username, login.password);
-      if (user != null) {
-        
-        return this.authService.login(user);
-      }
-      throw new UnauthorizedException();
+  @Post("login")
+  async login(@Body() login: LoginDto, @I18n() i18n: I18nContext) {
+    const user = await this.authService.validateUser(
+      login.username,
+      login.password
+    );
+    if (user != null) {
+      return this.authService.login(user);
     }
-
+    throw new UnauthorizedException();
+  }
 }
