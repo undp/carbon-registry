51fb97477a64db5b86f5b8eb97b2baec850d8689
dhanushkaxep <dhanushkap@xeptagon.com>
2023-03-07 12:51:08 +0530

    issue fixes

--- web/src/Components/Maps/MapboxComponent.tsx
+++ web/src/Components/Maps/MapboxComponent.tsx
@@ -9,7 +9,7 @@ import './MapboxComponent.scss';
 
 mapboxgl.accessToken = process.env.MAPBOXGL_ACCESS_TOKEN
   ? process.env.MAPBOXGL_ACCESS_TOKEN
-  : 'pk.eyJ1IjoicGFsaW5kYSIsImEiOiJjbGMyNTdqcWEwZHBoM3FxdHhlYTN4ZmF6In0.KBvFaMTjzzvoRCr1Z1dN_g';
+  : '';
 
 const MapboxComponent = (props: MapComponentProps) => {
   const mapContainerRef = useRef(null);
