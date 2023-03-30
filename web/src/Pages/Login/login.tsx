0d8f841e9c625cb7d44bf3ecb86c48a0ba87107a
Yathurshan Thurairajah <yathurshant@xeptagon.com>
2023-03-30 16:32:20 +0530

    Fixes for session timeout validation msg issue and changes in reset password page

--- web/src/Pages/Login/login.tsx
+++ web/src/Pages/Login/login.tsx
@@ -22,7 +22,7 @@ export interface LoginPageProps {
 const Login: FC<LoginPageProps> = (props: LoginPageProps) => {
   const { forgotPassword, resetPassword } = props;
   const { post, updateToken, removeToken } = useConnection();
-  const { IsAuthenticated, setUserInfo, isTokenExpired } = useUserContext();
+  const { IsAuthenticated, setUserInfo, isTokenExpired, setIsTokenExpired } = useUserContext();
   const { i18n, t } = useTranslation(['common', 'login']);
   const [loading, setLoading] = useState<boolean>(false);
   const [showError, setShowError] = useState<boolean>(false);
@@ -73,6 +73,7 @@ const Login: FC<LoginPageProps> = (props: LoginPageProps) => {
           IsAuthenticated(),
           IsAuthenticated(response.data.access_token)
         );
+        setIsTokenExpired(false);
         return IsAuthenticated(response.data.access_token)
           ? navigate(redirectLocation ? redirectLocation : '/dashboard', { replace: true })
           : navigate('/login');
