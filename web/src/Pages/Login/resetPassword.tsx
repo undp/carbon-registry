1f72c2701af7edc3e6ee17e34dbda58996a98cae
Yathurshan Thurairajah <yathurshant@xeptagon.com>
2023-03-31 10:29:46 +0530

    CHanges in reset password validation

--- web/src/Pages/Login/resetPassword.tsx
+++ web/src/Pages/Login/resetPassword.tsx
@@ -111,10 +111,10 @@ const ResetPassword: FC<ResetPasswordPageProps> = (props: ResetPasswordPageProps
                       ) {
                         throw new Error(`Confirm New Password ${t('common:isRequired')}`);
                       } else {
-                        const val = value.trim();
+                        const val = value;
                         const password = resetPasswordForm.getFieldValue('password');
                         if (password) {
-                          if (password.trim() !== val) {
+                          if (password !== val) {
                             throw new Error(`${t('resetPassword:passwordNotMatch')}`);
                           }
                         }
