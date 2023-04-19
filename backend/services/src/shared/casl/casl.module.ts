35578a6a881197319fdfc494d17ca48e88eca44a
Yathurshan Thurairajah <yathurshant@xeptagon.com>
2023-04-19 16:00:55 +0530

    changes in casl module imports

--- backend/services/src/shared/casl/casl.module.ts
+++ backend/services/src/shared/casl/casl.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common';
-import { CaslAbilityFactory } from './casl-ability.factory';
+import { UtilModule } from "../util/util.module";
 
 @Module({
+  imports: [UtilModule],
     providers: [CaslAbilityFactory],
     exports: [CaslAbilityFactory],
 })
