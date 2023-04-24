d7568937b85043ceb423e6abd2d5019d4c1acc3e
dhanushkaxep <dhanushkap@xeptagon.com>
2023-04-24 12:33:47 +0530

    fixed refactor issue

--- web/src/Pages/ProgrammeView/programmeView.tsx
+++ web/src/Pages/ProgrammeView/programmeView.tsx
@@ -1289,7 +1289,7 @@ const ProgrammeView = () => {
   const getFileName = (filepath: string) => {
     const index = filepath.indexOf('?');
     if (index > 0) {
-      filepath = filepath.substring(0, filepath.indexOf('?'));
+      filepath = filepath.substring(0, index);
     }
     const lastCharcter = filepath.charAt(filepath.length - 1);
     if (lastCharcter === '/') {
