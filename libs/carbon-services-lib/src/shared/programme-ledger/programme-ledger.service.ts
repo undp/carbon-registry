aeb5b89ebd5e906fa138c77ba6f5b7a027e3189e
Dhanuxeptagon <dhanushkapeiris@xeptagon.com>
2023-07-21 12:17:29 +0530

    fix issue in credit frozen update

--- libs/carbon-services-lib/src/shared/programme-ledger/programme-ledger.service.ts
+++ libs/carbon-services-lib/src/shared/programme-ledger/programme-ledger.service.ts
@@ -1662,6 +1662,7 @@ export class ProgrammeLedgerService {
           proponentPercentage: programme.proponentPercentage,
           companyId: programme.companyId,
           creditOwnerPercentage: programme.creditOwnerPercentage,
+          creditFrozen: programme.creditFrozen
         };
 
         updatedProgramme = programme;
