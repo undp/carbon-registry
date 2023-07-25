748a9c526c0e77f581d0f722737b54ce41db5b27
Dhanuxeptagon <dhanushkapeiris@xeptagon.com>
2023-07-25 10:07:15 +0530

    fix label display issue

--- web/src/Definitions/InterfacesAndType/programme.definitions.tsx
+++ web/src/Definitions/InterfacesAndType/programme.definitions.tsx
@@ -66,6 +66,24 @@ export enum SectoralScope {
   Agriculture = '15',
 }
 
+export enum SectoralScopeDisplayNames {
+  'Energy Industries' = '1',
+  'Energy Distribution' = '2',
+  'Energy Demand' = '3',
+  'Manufacturing Industries' = '4',
+  'Chemical Industries' = '5',
+  'Construction' = '6',
+  'Transport' = '7',
+  'Mining/Mineral Production' = '8',
+  'Metal Production' = '9',
+  'Fugitive Emissions From Fuels (Solid, Oil and Gas)' = '10',
+  'Fugitive Emissions From Production and Consumption of Halocarbons and Sulphur Hexafluoride' = '11',
+  'Solvent Use' = '12',
+  'Waste Handling and Disposal' = '13',
+  'Afforestation and Reforestation' = '14',
+  'Agriculture' = '15',
+}
+
 export enum TypeOfMitigation {
   AGRICULTURE = 'Agriculture',
   SOLAR = 'Solar',
@@ -216,8 +234,10 @@ export const getGeneralFields = (programme: Programme) => {
     applicationType: 'Programme Developer',
     sector: programme.sector,
     sectoralScope:
-      Object.keys(SectoralScope)[
-        Object.values(SectoralScope).indexOf(programme.sectoralScope as SectoralScope)
+      Object.keys(SectoralScopeDisplayNames)[
+        Object.values(SectoralScopeDisplayNames).indexOf(
+          programme.sectoralScope as SectoralScopeDisplayNames
+        )
       ],
     startDate: DateTime.fromSeconds(Number(programme.startTime)),
     endDate: DateTime.fromSeconds(Number(programme.endTime)),
