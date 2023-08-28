44cbf84b9651c773ce0d441b1a63af3cc2cfc964
gayanath8 <gayanathr@xeptagon.com>
2023-08-28 11:40:44 +0530

    add scraping certifier

--- libs/carbon-services-lib/src/data-importer/data-importer.service.ts
+++ libs/carbon-services-lib/src/data-importer/data-importer.service.ts
@@ -1,14 +1,19 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 import { ImporterInterface } from "./importer.interface";
 import { ITMOSystemImporter } from "./importers/itmo-system.service";
+import { CertifierScrapeService } from "./importers/certifier-scrape.service";
 import { ProgrammeService } from "../shared/programme/programme.service";
 import { UserService } from "../shared/user/user.service";
 import { CompanyService } from "../shared/company/company.service";
+import { Company } from "../shared/entities/company.entity";
 
 @Injectable()
 export class DataImporterService {
   constructor(
+    @InjectRepository(Company) private companyRepo: Repository<Company>,
     private logger: Logger,
     private configService: ConfigService,
     private companyService: CompanyService,
@@ -20,6 +25,10 @@ export class DataImporterService {
     switch(type) {
       case 'ITMO_SYSTEM':
         return new ITMOSystemImporter(this.logger, this.configService, this.companyService, this.userService, this.programmeService);
+    }
+    switch(type) {
+      case 'CERTIFIER_SCRAPING':
+        return new CertifierScrapeService(this.companyRepo, this.logger, this.configService, this.companyService, this.userService);
     }
     return null;
   }
