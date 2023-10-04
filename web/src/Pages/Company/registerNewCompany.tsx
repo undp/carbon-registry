d4b532126ab9fa201d068546fa3958c28d1adc39
tharindugayanga <tharindug@xeptagon.com>
2023-10-04 12:58:55 +0530

    NAM-1 NAM-2 adding company registration

---
+++ web/src/Pages/Company/registerNewCompany.tsx
@@ -0,0 +1,33 @@
+import { useConnection } from '../../Context/ConnectionContext/connectionContext';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { useUserContext } from '../../Context/UserInformationContext/userInformationContext';
+import { AddNewCompanyComponent } from '@undp/carbon-library';
+
+const RegisterNewCompany = () => {
+  const navigate = useNavigate();
+  const { t } = useTranslation(['addCompany']);
+
+  const maximumImageSize = process.env.REACT_APP_MAXIMUM_FILE_SIZE
+    ? parseInt(process.env.REACT_APP_MAXIMUM_FILE_SIZE)
+    : 5000000;
+
+  const onNavigateToHome = () => {
+    navigate('/', { replace: true });
+  };
+
+  return (
+    <AddNewCompanyComponent
+      t={t}
+      maximumImageSize={maximumImageSize}
+      useConnection={useConnection}
+      useUserContext={useUserContext}
+      useLocation={useLocation}
+      regionField
+      isGuest={true}
+      onNavigateToHome={onNavigateToHome}
+    ></AddNewCompanyComponent>
+  );
+};
+
+export default RegisterNewCompany;
