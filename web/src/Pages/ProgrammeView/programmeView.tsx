1ac95344476ae4c12b69e5f48bea95886380ae86
gayanath8 <gayanathr@xeptagon.com>
2023-10-12 10:56:34 +0530

    1 Day fix

--- web/src/Pages/ProgrammeView/programmeView.tsx
+++ web/src/Pages/ProgrammeView/programmeView.tsx
@@ -509,7 +509,16 @@ const ProgrammeView = () => {
         let el = undefined;
         let newEl = undefined;
         let creditEl = undefined;
+        let upcomingAuthorisation: any;
+        const day = Math.floor(
+          DateTime.now().diff(DateTime.fromMillis(activity.data.txTime), 'days').days
+        );
         if (activity.data.txType === TxType.CREATE) {
+          if (day === 1) {
+            upcomingAuthorisation = `Awaiting Action : ${day} Day`;
+          } else {
+            upcomingAuthorisation = `Awaiting Action : ${day} Days`;
+          }
           el = {
             status: 'process',
             title: t('view:tlCreate'),
@@ -524,9 +533,7 @@ const ProgrammeView = () => {
           newEl = {
             status: 'process',
             title: t('view:tlAuthorisation'),
-            subTitle: `Awaiting Action : ${Math.round(
-              DateTime.now().diff(DateTime.fromMillis(activity.data.txTime), 'days').days
-            )} Days`,
+            subTitle: upcomingAuthorisation,
             icon: (
               <span className="step-icon upcom-auth-step">
                 <Icon.ClipboardCheck />
