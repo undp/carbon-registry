1ec87cd144539bd6d63f5cf89887089de1913cbb
tharindugayanga <tharindug@xeptagon.com>
2023-10-17 13:24:56 +0530

    NAM-1 NAM-2 bugfix code refactor

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -70,7 +70,9 @@ const App = () => {
     <AbilityContext.Provider value={ability}>
       <ConnectionContextProvider
         serverURL={
-          process.env.REACT_APP_BACKEND ? process.env.REACT_APP_BACKEND : 'http://localhost:3000'
+          process.env.REACT_APP_BACKEND
+            ? process.env.REACT_APP_BACKEND
+            : 'http://localhost:3000/local'
         }
       >
         <UserInformationContextProvider>
