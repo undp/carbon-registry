4a6d47cc45cf5e80bf5babb4e3b8543b07e4f967
gayanath8 <gayanathr@xeptagon.com>
2023-11-30 08:27:57 +0530

    entrypoint.sh

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -1,46 +1,38 @@
 #!/bin/sh
+#!/bin/sh
 # vim:sw=4:ts=4:et
 
 set -e
 
-entrypoint_log() {
-    if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
-        echo "$@"
-    fi
-}
+if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+    exec 3>&1
+else
+    exec 3>/dev/null
+fi
 
 if [ "$1" = "nginx" -o "$1" = "nginx-debug" ]; then
     if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
-        entrypoint_log "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
+        echo >&3 "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
 
-        entrypoint_log "$0: Looking for shell scripts in /docker-entrypoint.d/"
-        find "/docker-entrypoint.d/" -follow -type f -print | sort -V | while read -r f; do
+        echo >&3 "$0: Looking for shell scripts in /docker-entrypoint.d/"
+        find "/docker-entrypoint.d/" -follow -type f -print | sort -n | while read -r f; do
             case "$f" in
-                *.envsh)
-                    if [ -x "$f" ]; then
-                        entrypoint_log "$0: Sourcing $f";
-                        . "$f"
-                    else
-                        # warn on shell scripts without exec bit
-                        entrypoint_log "$0: Ignoring $f, not executable";
-                    fi
-                    ;;
                 *.sh)
                     if [ -x "$f" ]; then
-                        entrypoint_log "$0: Launching $f";
+                        echo >&3 "$0: Launching $f";
                         "$f"
                     else
                         # warn on shell scripts without exec bit
-                        entrypoint_log "$0: Ignoring $f, not executable";
+                        echo >&3 "$0: Ignoring $f, not executable";
                     fi
                     ;;
-                *) entrypoint_log "$0: Ignoring $f";;
+                *) echo >&3 "$0: Ignoring $f";;
             esac
         done
 
-        entrypoint_log "$0: Configuration complete; ready for start up"
+        echo >&3 "$0: Configuration complete; ready for start up"
     else
-        entrypoint_log "$0: No files found in /docker-entrypoint.d/, skipping configuration"
+        echo >&3 "$0: No files found in /docker-entrypoint.d/, skipping configuration"
     fi
 fi
 
