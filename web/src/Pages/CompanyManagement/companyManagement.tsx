c28f51cfd72fb18444836ae021a585c6e6c04357
leshanw <leshanw@xeptagon.com>
2023-12-05 12:58:38 +0530

    CARBON-382-U : Removed post prop from CompanyManagementComponent

--- web/src/Pages/CompanyManagement/companyManagement.tsx
+++ web/src/Pages/CompanyManagement/companyManagement.tsx
@@ -1,11 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useAbilityContext } from '../../Casl/Can';
-import { CompanyManagementComponent, CompanyManagementColumns, useConnection } from '@undp/carbon-library';
+import { CompanyManagementComponent, CompanyManagementColumns } from '@undp/carbon-library';
 
 const CompanyManagement = () => {
   const navigate = useNavigate();
-  const { post } = useConnection();
   const { t } = useTranslation(['company', 'companyProfile']);
 
   const visibleColumns = [
@@ -29,7 +28,6 @@ const CompanyManagement = () => {
     <CompanyManagementComponent
       t={t}
       useAbilityContext={useAbilityContext}
-      post={post}
       visibleColumns={visibleColumns}
       onNavigateToCompanyProfile={navigateToCompanyProfile}
       onClickAddCompany={navigateToAddNewCompany}
