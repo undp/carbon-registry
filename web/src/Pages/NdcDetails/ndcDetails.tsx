793f185b4680779cc7f869969343c7f9b26142d0
lochana <lochanaf@xeptagon.com>
2024-01-02 13:22:49 +0530

    ndcDetails import error fix

--- web/src/Pages/NdcDetails/ndcDetails.tsx
+++ web/src/Pages/NdcDetails/ndcDetails.tsx
@@ -1,8 +1,6 @@
-import { useConnection } from '../../Context/ConnectionContext/connectionContext';
-import { useUserContext } from '../../Context/UserInformationContext/userInformationContext';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { NdcDetailsComponent } from '@undp/carbon-library';
+import { NdcDetailsComponent, useConnection, useUserContext } from '@undp/carbon-library';
 
 const NdcDetails = () => {
   const navigate = useNavigate();
