7781140f0850e8e9f25775fb775aa444d7ba0abf
gayanath8 <gayanathr@xeptagon.com>
2024-01-05 12:00:21 +0530

    System Notifications

--- web/src/Pages/ProgrammeView/programmeView.tsx
+++ web/src/Pages/ProgrammeView/programmeView.tsx
@@ -1334,7 +1334,7 @@ const ProgrammeView = () => {
                 : action === 'Issue'
                 ? 'Successfully issued'
                 : action === 'Certify'
-                ? 'Successfully certified'
+                ? 'The programme has been certified successfully '
                 : action === 'Revoke'
                 ? t('view:successRevokeCertifcate')
                 : t('view:successRetire'),
