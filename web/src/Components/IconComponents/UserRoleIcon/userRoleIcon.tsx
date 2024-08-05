5542c47276260119e70375ff6426064711b64331
tharindugayanga <tharindug@xeptagon.com>
2024-08-05 11:34:05 +0530

    Add calculator lib to FE

--- web/src/Components/IconComponents/UserRoleIcon/userRoleIcon.tsx
+++ web/src/Components/IconComponents/UserRoleIcon/userRoleIcon.tsx
@@ -1,10 +1,5 @@
-import {
-  EyeOutlined,
-  SearchOutlined,
-  StarOutlined,
-  ToolOutlined,
-} from "@ant-design/icons";
-import React, { FC } from "react";
+import { EyeOutlined, SearchOutlined, StarOutlined, ToolOutlined } from '@ant-design/icons';
+import React, { FC } from 'react';
 import {
   AdminBGColor,
   AdminColor,
@@ -14,8 +9,8 @@ import {
   RootColor,
   ViewBGColor,
   ViewColor,
-} from "../../../Styles/role.color.constants";
-import { RoleIcon } from "../RoleIcon/role.icon";
+} from '../../../Styles/role.color.constants';
+import { RoleIcon } from '../RoleIcon/role.icon';
 
 export interface UserRoleIconProps {
   role: string;
@@ -24,32 +19,17 @@ export interface UserRoleIconProps {
 export const UserRoleIcon: FC<UserRoleIconProps> = (props: UserRoleIconProps) => {
   const { role } = props;
   return (
-    <div
-      style={{ display: "flex", alignItems: "center", flexDirection: "row" }}
-    >
-      {role === "Admin" ? (
-        <RoleIcon
-          icon={<StarOutlined />}
-          bg={AdminBGColor}
-          color={AdminColor}
-        />
-      ) : role === "Root" ? (
-        <RoleIcon
-          icon={<SearchOutlined />}
-          bg={RootBGColor}
-          color={RootColor}
-        />
-      ) : role === "Manager" ? (
-        <RoleIcon
-          icon={<ToolOutlined />}
-          bg={ManagerBGColor}
-          color={ManagerColor}
-        />
+    <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
+      {role === 'Admin' ? (
+        <RoleIcon icon={<StarOutlined />} bg={AdminBGColor} color={AdminColor} />
+      ) : role === 'Root' ? (
+        <RoleIcon icon={<SearchOutlined />} bg={RootBGColor} color={RootColor} />
+      ) : role === 'Manager' ? (
+        <RoleIcon icon={<ToolOutlined />} bg={ManagerBGColor} color={ManagerColor} />
       ) : (
         <RoleIcon icon={<EyeOutlined />} bg={ViewBGColor} color={ViewColor} />
       )}
-      <div>{role === "ViewOnly" ? "Viewer" : role}</div>
+      <div>{role === 'ViewOnly' ? 'Viewer' : role}</div>
     </div>
   );
 };
-
