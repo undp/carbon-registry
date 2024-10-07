9072a4d06feaefb6ba6834b3999da3f1527371fb
tharindugayanga <tharindug@xeptagon.com>
2024-10-07 11:02:00 +0530

    Code review fixes

--- backend/services/src/national-api/national.api.module.ts
+++ backend/services/src/national-api/national.api.module.ts
@@ -1,35 +1,34 @@
-import { Logger, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { NationalAPIController } from './national.api.controller';
-import { NationalAPIService } from './national.api.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { CompanyController } from './company.controller';
-import { UserController } from './user.controller';
-import { AuthController } from './auth.controller';
-import { ProgrammeController } from './programme.controller';
-import { SettingsController } from './settings.controller';
-import { AuthModule } from '../auth/auth.module';
-import { CaslModule } from '../casl/casl.module';
-import { CompanyModule } from '../company/company.module';
-import { ProgrammeModule } from '../programme/programme.module';
-import { TypeOrmConfigService } from '../typeorm.config.service';
-import { UserModule } from '../user/user.module';
-import { UtilModule } from '../util/util.module';
-import configuration from '../configuration';
-import { LocationModule } from '../location/location.module';
-import { LocationController } from './location.controller';
-
+import { Logger, Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { NationalAPIController } from "./national.api.controller";
+import { NationalAPIService } from "./national.api.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CompanyController } from "./company.controller";
+import { UserController } from "./user.controller";
+import { AuthController } from "./auth.controller";
+import { ProgrammeController } from "./programme.controller";
+import { SettingsController } from "./settings.controller";
+import { AuthModule } from "../auth/auth.module";
+import { CaslModule } from "../casl/casl.module";
+import { CompanyModule } from "../company/company.module";
+import { ProgrammeModule } from "../programme/programme.module";
+import { TypeOrmConfigService } from "../typeorm.config.service";
+import { UserModule } from "../user/user.module";
+import { UtilModule } from "../util/util.module";
+import configuration from "../configuration";
+import { LocationModule } from "../location/location.module";
+import { LocationController } from "./location.controller";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
-      envFilePath: [`.env.${process.env.NODE_ENV}`, `.env`]
+      envFilePath: [`.env.${process.env.NODE_ENV}`, `.env`],
     }),
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
-      imports: undefined
+      imports: undefined,
     }),
     AuthModule,
     UserModule,
@@ -37,20 +36,17 @@ import { LocationController } from './location.controller';
     ProgrammeModule,
     CompanyModule,
     UtilModule,
-    LocationModule
+    LocationModule,
   ],
-  controllers: [ 
-		NationalAPIController, 
-		UserController, 
-		AuthController, 
-		CompanyController, 
-		ProgrammeController, 
-		SettingsController,
-    LocationController 
-	 ],
-  providers: [
-    NationalAPIService, 
-    Logger
+  controllers: [
+    NationalAPIController,
+    UserController,
+    AuthController,
+    CompanyController,
+    ProgrammeController,
+    SettingsController,
+    LocationController,
   ],
+  providers: [NationalAPIService, Logger],
 })
 export class NationalAPIModule {}
